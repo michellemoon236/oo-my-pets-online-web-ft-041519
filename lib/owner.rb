920f21ead1dbfd5d6f69e57b5d308601d3d53f2a
Michelle Moon <michellemoon236@gmail.com>
2019-04-26 02:43:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -13,7 +13,8 @@ class Owner
     @pets[@dogs] = []
     @pets[@cats] = []
     @pets[@fish] = []
-
+      #is initialized with a pets attribute as a hash with 3 keys (FAILED - 23)
+      #initializes with a species (FAILED - 19)
   end 
   def species
 
