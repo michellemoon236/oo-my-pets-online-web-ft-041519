84deb9672c0af150af714e9a6c25199f434e1ad7
Michelle Moon <michellemoon236@gmail.com>
2019-04-26 02:51:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,63 +1,79 @@
 class Owner
 
-  #Class methods
-    ::all returns all instances of Owner that have been created (FAILED - 16)
-    ::count returns the number of owners that have been created (FAILED - 17)
-    ::reset_all can reset the owners that have been created (FAILED - 18)
+  attr_accessor :name
+  attr_reader :species
   
+  @@all = []
+  #Class methods
+  
+  def self.all
+    #all returns all instances of Owner that have been created (FAILED - 16)
+    @@all
+  end 
+
+  def self.count
+    #count returns the number of owners that have been created (FAILED - 17)
+    @@all.count
+  end
+    
+  def self.reset_all
+    #reset_all can reset the owners that have been created (FAILED - 18)
+    @@all.clear
+  end 
   
   #Instance methods
+  
   def initialize(species)
+    #is initialized with a pets attribute as a hash with 3 keys (FAILED - 23)
+    #initializes with a species (FAILED - 19)   @species = species
     @species = species
+    @@all << self
     @pets = {}
     @pets[@dogs] = []
     @pets[@cats] = []
     @pets[@fish] = []
-      #is initialized with a pets attribute as a hash with 3 keys (FAILED - 23)
-      #initializes with a species (FAILED - 19)
   end 
-  def species
-
-      #can't change its species (FAILED - 20)
-  end
+  
   
   def say_species
       #can say its species (FAILED - 21)
   end
   
-  def name
-      #can have a name (FAILED - 22)
-  end
-  
-  def pets
-
-  end
-  
   def buy_fish
-      #can buy a fish that is an instance of the Fish class (FAILED - 24)
-      #knows about its fishes (FAILED - 25)
+    #can buy a fish that is an instance of the Fish class (FAILED - 24)
+    #knows about its fishes (FAILED - 25)
+    fish = Fish.new
+    @pets[@fish] << fish
   end
   
   def buy_cat
-      #can buy a cat that is an instance of the Cat class (FAILED - 26)
-      #knows about its cats (FAILED - 27)
+    #can buy a cat that is an instance of the Cat class (FAILED - 26)
+    #knows about its cats (FAILED - 27)
+    cat = Cat.new
+    @pets[@cats] << cat 
   end
   
   def buy_dog
-      #can buy a dog that is an instance of the Dog class (FAILED - 28)
-      #knows about its dogs (FAILED - 29)
+    #can buy a dog that is an instance of the Dog class (FAILED - 28)
+    #knows about its dogs (FAILED - 29)
+    dog = Dog.new 
+    @pets[@dogs] << dogs
+      
   end
   
   def walk_dogs
       #walks the dogs which makes the dogs' moods happy (FAILED - 30)
+    Dog.mood = "happy"
   end
   
   def play_with_cats
       #plays with the cats which makes the cats moods happy (FAILED - 31)
+    Cat.mood = "happy"
   end
   
   def feed_fish
       #feeds the fishes which makes the fishes' moods happy (FAILED - 32)
+    Fish.modd = "happy"
   end
   
   def sell_pets
@@ -66,6 +82,7 @@ class Owner
   
   def list_pets
       #can list off its pets (FAILED - 34)
+    @pets 
   end
 
 end
