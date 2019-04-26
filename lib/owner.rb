265fabc247bd73607d9f9b2cb542f7bdc7d83cc8
Michelle Moon <michellemoon236@gmail.com>
2019-04-26 02:39:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
 class Owner
-  # cod
+
+  #Class methods
+    ::all returns all instances of Owner that have been created (FAILED - 16)
+    ::count returns the number of owners that have been created (FAILED - 17)
+    ::reset_all can reset the owners that have been created (FAILED - 18)
+  
+  
+  #Instance methods
+  def species
+      initializes with a species (FAILED - 19)
+      can't change its species (FAILED - 20)
+  def say_species
+      can say its species (FAILED - 21)
+  def name
+      can have a name (FAILED - 22)
+  def pets
+      is initialized with a pets attribute as a hash with 3 keys (FAILED - 23)
+  def buy_fish
+      can buy a fish that is an instance of the Fish class (FAILED - 24)
+      knows about its fishes (FAILED - 25)
+    #buy_cat
+      can buy a cat that is an instance of the Cat class (FAILED - 26)
+      knows about its cats (FAILED - 27)
+    #buy_dog
+      can buy a dog that is an instance of the Dog class (FAILED - 28)
+      knows about its dogs (FAILED - 29)
+    #walk_dogs
+      walks the dogs which makes the dogs' moods happy (FAILED - 30)
+    #play_with_cats
+      plays with the cats which makes the cats moods happy (FAILED - 31)
+    #feed_fish
+      feeds the fishes which makes the fishes' moods happy (FAILED - 32)
+    #sell_pets
+      can sell all its pets, which make them nervous (FAILED - 33)
+    #list_pets
+      can list off its pets (FAILED - 34)
+
+
 end
